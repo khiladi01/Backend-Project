9514e066830c9d6dd81cc8a795ee1a0e4b9bd1e9
raushan778 <allmusiccenter404@gmail.com>
2025-03-21 21:55:52 +0530

    comment in asynchandler

--- src/utils/asynchandler.js
+++ src/utils/asynchandler.js
@@ -1,22 +1,15 @@
-const asynchandler = (requesthandler) => {
-    
+// Desc: Async handler for express routes
+const asynchandler = (fn) => async (req,res,next) => {
+    try {
+        await fn(req,res,next)
+    } catch (error) {
+        res.status((error.code || 500).json({
+            success: false,
+            message:error.message
+        }))
+    }
 }
-
-
-
-
 
 export { asynchandler }
 
-
-// const asynchandler = (fn) => async (req,res,next) => {
-//     try {
-//         await fn(req,res,next)
-//     } catch (error) {
-//         res.status((error.code || 500).json({
-//             success: false,
-//             mesage:error.message
-//         }))
-//     }
-// }
-
+// This utility function streamlines the error-handling process for asynchronous route handlers, ensuring that any unhandled promise rejections are caught and properly processed. It's a clean and efficient way to manage async code in Express.js!! Let me know if you'd like further tips or insights.
